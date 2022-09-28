9869b6297dae1b21844d77ed6a706674c60b3ce9
Rifat <mdrifatahmed787@gmail.com>
2022-09-29 04:03:35 +0600

    break added

--- src/Components/Players/Players.js
+++ src/Components/Players/Players.js
@@ -26,13 +26,35 @@ const Players = () => {
             <div className='shadow-xl bg-stone-100'>
                 <h1 className='font-bold text-2xl text-center mt-10'>Md Rifat</h1>
                 <p className='font-bold text-base text-center mt-2'>Dhaka, Bangladesh</p>
-                <div className="overflow-x-auto">
+                <div className="overflow-x-auto mx-3">
                     <table className="table w-full mt-4">
                         <tbody>
                             <tr className="active">
                                 <td><span className='font-bold text-2xl'>75</span>kg <br />Weight </td>
                                 <td><span className='font-bold text-2xl'>5.6</span><br />Height</td>
                                 <td><span className='font-bold text-2xl'>24</span>yrs <br />Age</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="overflow-x-auto mx-3">
+                    <h1 className='font-bold text-2xl text-center mt-10'>Add A Break</h1>
+                    <table className="table w-full mt-4">
+                        <tbody>
+                            <tr className="active">
+                                <td>
+                                    <button className="btn btn-circle  btn-outline">10 <span className='lowercase'>min</span></button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-circle btn-outline">15 <span className='lowercase'>min</span></button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-circle btn-outline">20 <span className='lowercase'>min</span></button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-circle btn-outline">30 <span className='lowercase'>min</span></button>
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
