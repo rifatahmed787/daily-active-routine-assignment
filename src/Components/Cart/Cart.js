a76c5265a697a2381686d9768dbe9aded58893d1
Rifat <mdrifatahmed787@gmail.com>
2022-09-29 10:39:52 +0600

    toast added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const Cart = ({ cart }) => {
     // console.log(cart)
@@ -11,13 +12,20 @@ const Cart = ({ cart }) => {
         // console.log(total)
     }
 
+    const AddGoodJob = () => {
+        Swal.fire(
+            'Good job!',
+            'You completed your activities!',
+            'success'
+        )
+    }
 
     return (
         <div>
             <h2 className='mt-10 font-bold text-xl text-center'>Activities Details</h2>
             <h3 className='mt-6 font-bold text-lg ml-3 border bottom-1 p-5 bg-slate-400 rounded-lg'>Activities Time: {total} minutes</h3>
             <h3 className='mt-6 font-bold text-lg ml-3 border bottom-1 p-5 bg-slate-400 rounded-lg'>Break Time:</h3>
-            <button className="btn btn-wide btn-primary mt-20 ml-10">Activity Completed</button>
+            <button onClick={AddGoodJob} className="btn btn-wide btn-primary mt-20 ml-10">Activity Completed</button>
         </div>
     );
 };
